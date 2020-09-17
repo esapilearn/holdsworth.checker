0418d52c7a32cd4ea8fcc584586fcad4e8c3bb69
Clay Holdsworth <CHOLDSWORTH@BWH.HARVARD.EDU>
2020-09-17 10:25:26 -0700

    working

---
+++ Holdsworth.Checker/ViewModels/PlanConstraint.cs
@@ -0,0 +1,29 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace Holdsworth.Checker.ViewModels
+{
+    public class PlanConstraint : BindableBase
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+        private IConstraint _constraint;
+
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
