3ac0d25fa798111e53a972a1074ac535d19f0376
Clay Holdsworth <CHOLDSWORTH@BWH.HARVARD.EDU>
2020-09-17 09:32:31 -0700

    Add project files.

---
+++ Holdsworth.Checker/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Holdsworth.Checker.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
