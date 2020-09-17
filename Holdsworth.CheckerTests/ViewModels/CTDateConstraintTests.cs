135d001b08cc399cdfc40b65bcce39800981f659
Clay Holdsworth <CHOLDSWORTH@BWH.HARVARD.EDU>
2020-09-17 11:20:16 -0700

    Unit test

---
+++ Holdsworth.CheckerTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cardan.PlanChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace Cardan.PlanChecker.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void CTDateConstraintFailsCorrectly()
+        {
+            //Arrange
+            var ctDate = DateTime.Now.AddDays(-61);
+            //Act
+            var result = new CTDateConstraint().Constrain(ctDate).ResultType;
+            //Assert
+            Assert.AreEqual(ResultType.ACTION_LEVEL_3, result);
+        }
+
+        [TestMethod()]
+        public void CTDateConstraintPassesCorrectly()
+        {
+            //Arrange
+            var ctDate = DateTime.Now.AddDays(-59);
+            //Act
+            var result = new CTDateConstraint().Constrain(ctDate).ResultType;
+            //Assert
+            Assert.AreEqual(ResultType.PASSED, result);
+        }
+    }
+}
